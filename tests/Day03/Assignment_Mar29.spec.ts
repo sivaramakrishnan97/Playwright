36a8115e4d63c0eff2147fdbc7492730b104b9ff
sivaramakrishnan D <sivaraman.drs@gmail.com>
2025-06-07 16:12:42 +0530

    second commit

--- tests/Day03/Assignment_Mar29.spec.ts
+++ tests/Day03/Assignment_Mar29.spec.ts
@@ -4,9 +4,8 @@ import { channel } from 'process';
 test(`Launch browser test`, async ({})=>{
 
 // Launch website 
-/* 
 const browserr = await chromium.launch({headless:false, channel: "chrome"});
-const context = await browser.newContext()                                          // context = window
+const context = await browserr.newContext()                                          // context = window
 const page = await context.newPage()                                                // Page = Tab
 
 await page.goto("https://www.flipkart.com")
@@ -16,23 +15,6 @@ console.log(`the URL of the page is ${url}`)
 
 const title = await page.title()
 console.log(`the title of the page is ${title}`) 
- */
-
-
-})
-
-
-test('browserTest', async()=>{
-test.setTimeout(60000)
-const browserr = await chromium.launch({headless:false, channel: "msedge"});
-
-const contextt = await browserr.newContext()
-
-const newPagee = await contextt.newPage();
-
-await newPagee.goto("https://www.flipkart.com")
-
-console.log("Browser launched");
 
 
 })
